940c524ddb3a47fae2005fd7b612cff973eab4be
sujin <ikosdu60@gmail.com>
2023-05-17 19:07:58 +0900

    78. Event Detail 페이지 추가하기

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,33 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import { Fragment } from "react";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 function EventDetailPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId; // router.query를 통해 eventId에 접근
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No Event found!</p>;
+  }
+  console.log(event);
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
 
